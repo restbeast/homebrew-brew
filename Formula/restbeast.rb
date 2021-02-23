0f331e149b4bb0fb1207b92c77a0a893e352164c
Çağrı Ekin <cagri.ekin@gmail.com>
2021-02-23 23:50:49 +0100

    Update restbeast.rb

--- Formula/restbeast.rb
+++ Formula/restbeast.rb
@@ -8,7 +8,14 @@ class Restbeast < Formula
   depends_on "go" => :build
 
   def install
-    system "make", "VERSION=v1.0.0"
+    system "go",
+      "build",
+      "-o",
+      "restbeast",
+      "-ldflags",
+      "-X 'main.version=v1.0.0' -X 'main.sentryDsn=https://9c22d42ad3594172b24f9f5fc4db9d73@o421531.ingest.sentry.io/5341476'",
+      "-v",
+      "-buildmode=exe"
     bin.install "restbeast"
   end
 
