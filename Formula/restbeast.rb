9cc6f8a50842bc3626247b6c3d86ded3418f6297
Çağrı Ekin <cagri.ekin@gmail.com>
2021-02-23 01:11:13 +0100

    Update restbeast.rb

--- Formula/restbeast.rb
+++ Formula/restbeast.rb
@@ -5,7 +5,7 @@ class Restbeast < Formula
   sha256 "86ce87f0cf14178131aad34a65508b33509729282e61d0f944594e26aea63ddd"
   license "GPL-3.0"
 
-  depends_on "go@1.16" => :build
+  depends_on "go@1.14" => :build
 
   def install
     system "CI_COMMIT_TAG=v1.0.0", 
