3ee9c44452b713d4a85dec759ccc536dfddd6472
Çağrı Ekin <cagri.ekin@gmail.com>
2021-11-09 11:32:26 +0100

    Update restbeast.rb

--- Formula/restbeast.rb
+++ Formula/restbeast.rb
@@ -1,8 +1,8 @@
 class Restbeast < Formula
   desc "Command-line API client, an API testing tool and an easy load testing tool"
   homepage "https://restbeast.com"
-  url "https://github.com/restbeast/restbeast/archive/v1.2.0.tar.gz"
-  sha256 "e795241105b688587764e75a63ff2bc04db4966c2ab16ab8671b0951262a0414"
+  url "https://github.com/restbeast/restbeast/archive/v1.3.0.tar.gz"
+  sha256 "aa088653f11c932acf89b0eda28f807a4a52605eeb1f78cc2162f4f8ab81a43b"
   license "GPL-3.0"
 
   depends_on "go" => :build
@@ -13,7 +13,7 @@ class Restbeast < Formula
       "-o",
       "restbeast",
       "-ldflags",
-      "-X 'main.version=v1.2.0' -X 'main.sentryDsn=https://9c22d42ad3594172b24f9f5fc4db9d73@o421531.ingest.sentry.io/5341476'",
+      "-X 'main.version=v1.3.0' -X 'main.sentryDsn=https://9c22d42ad3594172b24f9f5fc4db9d73@o421531.ingest.sentry.io/5341476'",
       "-v",
       "-buildmode=exe"
     bin.install "restbeast"
