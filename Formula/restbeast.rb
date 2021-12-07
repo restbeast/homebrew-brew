00e7fe71f58af7773ef3d36633f5bb29606651a8
Çağrı Ekin <cagri.ekin@gmail.com>
2021-12-07 22:09:01 +0100

    Update restbeast.rb

--- Formula/restbeast.rb
+++ Formula/restbeast.rb
@@ -13,7 +13,7 @@ class Restbeast < Formula
       "-o",
       "restbeast",
       "-ldflags",
-      "-X 'main.version=v1.3.0' -X 'main.sentryDsn=https://9c22d42ad3594172b24f9f5fc4db9d73@o421531.ingest.sentry.io/5341476'",
+      "-X 'main.version=v1.3.2' -X 'main.sentryDsn=https://9c22d42ad3594172b24f9f5fc4db9d73@o421531.ingest.sentry.io/5341476'",
       "-v",
       "-buildmode=exe"
     bin.install "restbeast"
